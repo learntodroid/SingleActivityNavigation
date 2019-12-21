a0127abe97d416054ac9f7824ad6a04d4a56e0e6
Jarrod <learntodroid1@gmail.com>
2019-12-21 19:49:21 +1100

    navigation between 2 fragments

--- app/src/main/java/com/learntodroid/singleactivitynavigation/Fragment1.java
+++ app/src/main/java/com/learntodroid/singleactivitynavigation/Fragment1.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
-public class Fragment1 extends Fragment {
+public class Fragment1 extends Fragment implements View.OnClickListener {
+    Button gotoFragment2;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +23,15 @@ public class Fragment1 extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_fragment1, container, false);
+
+        gotoFragment2 = view.findViewById(R.id.fragment_fragment1_gotofragment2);
+        gotoFragment2.setOnClickListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Navigation.findNavController(view).navigate(R.id.action_fragment1_to_fragment2);
     }
 }
